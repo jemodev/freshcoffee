21bdfa072da0c8e86f72f05d6367cd000e318f44
Jerson Moreno <jjmoreno@pan-energy.com>
2026-01-08 17:00:17 -0400

    feat: add ProductItem and Products components with schema validation

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,19 @@
 import { z } from "astro:content";
 
+const ImageSchema = z.object({
+    url: z.string().url(),
+    width: z.number(),
+    height: z.number(),
+});
+
+const FeaturedImageSchema = z.object({
+    thumbnail: ImageSchema,
+    medium: ImageSchema,
+    medium_large: ImageSchema,
+    large: ImageSchema,
+    full: ImageSchema,
+});
+
 const CategorySchema = z.object({
     id: z.number(),
     name: z.string(),
@@ -11,3 +25,21 @@ const CategorySchema = z.object({
 
 export const CategoriesSchema = z.array(CategorySchema);
 export type Category = z.infer<typeof CategorySchema>;
+
+const ProductSchema = z.object({
+    id: z.number(),
+    slug: z.string(),
+    title: z.object({
+        rendered: z.string(),
+    }),
+    featured_media: z.number(),
+    featured_images: FeaturedImageSchema,
+    // acf: z.object({
+    //     description: z.string(),
+    //     price: z.number(),
+    //     image: z.string().url(),
+    // }),
+});
+
+export const ProductsSchema = z.array(ProductSchema);
+export type Product = z.infer<typeof ProductSchema>;
