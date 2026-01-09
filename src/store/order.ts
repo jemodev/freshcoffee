e6b9f711ab2ddf3ed53a1d9cb7b1f8ff6518654c
jemodev <jemo.webdev@gmail.com>
2026-01-09 11:46:11 -0400

    feat: implement unique key handling for orders based on product size in addItem function

--- src/store/order.ts
+++ src/store/order.ts
@@ -1,4 +1,5 @@
 import type { OrderItem, SelectedProduct } from "@/types";
+import { toLowerFirstChar } from "@/utils";
 import { create } from "zustand";
 
 type Store = {
@@ -17,10 +18,31 @@ export const useOrderStore = create<Store>((set, get) => ({
     addItem: (product) => {
         const currentOrders = get().orders;
 
+        // Check if the product with the same id and size already exists
+        const hasSize = Boolean(product.size);
+        const key = hasSize
+            ? `${product.id}-${toLowerFirstChar(product.size!)}`
+            : undefined;
+
+        const existingOrderIndex = currentOrders.findIndex((order) =>
+            hasSize ? order.key === key : order.id === product.id
+        );
+
+        if (existingOrderIndex !== -1) {
+            const updatedOrders = [...currentOrders];
+            updatedOrders[existingOrderIndex].quantity += 1;
+            updatedOrders[existingOrderIndex].subtotal =
+                updatedOrders[existingOrderIndex].quantity *
+                updatedOrders[existingOrderIndex].price;
+            set({ orders: updatedOrders });
+            return;
+        }
+
         const newOrder = {
             ...product,
             quantity: 1,
             subtotal: product.price,
+            key,
         };
 
         set({ orders: [...currentOrders, newOrder] });
