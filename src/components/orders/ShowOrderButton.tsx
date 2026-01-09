eeef9b54ce7fcfe10b1ab2ee15e9c156efa72f55
jemodev <jemo.webdev@gmail.com>
2026-01-09 17:23:07 -0400

    feat: update ShowOrderButton to display total items in cart and improve layout

--- src/components/orders/ShowOrderButton.tsx
+++ src/components/orders/ShowOrderButton.tsx
@@ -2,15 +2,22 @@ import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 import { useOrderStore } from "@/store/order.ts";
 
 export default function ShowOrderButton() {
-    const { toggleOrderDrawer } = useOrderStore();
+    const { toggleOrderDrawer, orders } = useOrderStore();
+
+    const totalItems = orders.reduce((acc, order) => acc + order.quantity, 0);
 
     return (
         <button
             type="button"
-            className="cursor-pointer p-4"
+            className="cursor-pointer p-4 flex items-center gap-3"
             onClick={toggleOrderDrawer}
         >
             <ShoppingCartIcon className="size-8 text-gray-700 hover:text-gray-900" />
+            {totalItems > 0 && (
+                <span className="ml-2 rounded-full bg-amber-500 px-2 py-1 text-xs font-bold text-white">
+                    {totalItems}
+                </span>
+            )}
         </button>
     );
 }
